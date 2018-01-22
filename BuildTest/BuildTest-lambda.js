c2fde1925a395172f11c687f763675384cafcc6f
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-22 21:22:25 +0530

    init

---
+++ BuildTest/BuildTest-lambda.js
@@ -0,0 +1,22 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+
+	sns.publish({
+		Message: 'MyMessage',
+		MessageAttributes: {},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:udith_test'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
+
+
+
+	callback(null, 'Successfully executed');
+}
